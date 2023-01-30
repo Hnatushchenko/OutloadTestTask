282d7324bb77cc8fee9ad2df53b933733dd87f9c
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-30 09:57:20 +0200

    create RssSubscription model

---
+++ OutloadTestTaskApp/Models/RssSubscription.cs
@@ -0,0 +1,10 @@
+﻿namespace OutloadTestTaskApp.Models
+{
+    public class RssSubscription
+    {
+        public required Guid Id { get; set; }
+        public required string FeedUrl { get; set; }
+        public required bool IsRead { get; set; }
+        public required DateTimeOffset CreationDate { get; set; }
+    }
+}
