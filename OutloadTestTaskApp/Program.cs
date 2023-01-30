5dcf52a9c374b04b37e68b33d170f424ec2ba32b
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-30 10:04:24 +0200

    add connection string to appsettings and to Program.cs

--- OutloadTestTaskApp/Program.cs
+++ OutloadTestTaskApp/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OutloadTestTaskApp.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
